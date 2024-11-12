04140c708d496c8ff1f9ca7de72b1e29010896ba
Jose Guillermo Galvez Pacori <129212989+KarTaGo124@users.noreply.github.com>
2024-11-12 13:11:12 -0500

    Update scrap_table.py

--- scrap_table.py
+++ scrap_table.py
@@ -1,47 +1,69 @@
+import os
 import requests
 from bs4 import BeautifulSoup
-import boto3
 import uuid
+import boto3
 
 def lambda_handler(event, context):
-    # URL de la página web que contiene la tabla
-    url = "https://sgonorte.bomberosperu.gob.pe/24horas/?criterio=/"
+    # Obtén la clave de API de las variables de entorno
+    api_key = os.environ.get("API_KEY")
 
-    # Realizar la solicitud HTTP a la página web
-    response = requests.get(url)
-    if response.status_code != 200:
+    if not api_key:
         return {
-            'statusCode': response.status_code,
-            'body': 'Error al acceder a la página web'
+            'statusCode': 500,
+            'body': "La clave de API no está configurada en las variables de entorno"
         }
 
-    # Parsear el contenido HTML de la página web
-    soup = BeautifulSoup(response.content, 'html.parser')
+    # URL de la página que deseas scrapear
+    url = "https://ultimosismo.igp.gob.pe/ultimo-sismo/sismos-reportados"
 
-    # Encontrar la tabla en el HTML
-    table = soup.find('table')
+    # Construir la URL de ScraperAPI
+    scraperapi_url = f"https://api.scraperapi.com/?api_key={api_key}&url={url}&render=true"
+
+    try:
+        # Hacer la solicitud a ScraperAPI
+        response = requests.get(scraperapi_url)
+
+        if response.status_code != 200:
+            return {
+                'statusCode': response.status_code,
+                'body': f"Error al obtener la página: {response.text}"
+            }
+        html = response.text  # HTML renderizado
+        print(html)
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': f"Error al acceder a la página web: {str(e)}"
+        }
+
+    # Parsear el contenido HTML con BeautifulSoup
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Encontrar la tabla
+    table = soup.find("table")
     if not table:
         return {
             'statusCode': 404,
             'body': 'No se encontró la tabla en la página web'
         }
 
-    # Extraer los encabezados de la tabla
-    headers = [header.text for header in table.find_all('th')]
+    # Extraer encabezados
+    headers = [header.text.strip() for header in table.find_all('th')[:-1]]
 
-    # Extraer las filas de la tabla
+    # Extraer filas de la tabla
     rows = []
     for row in table.find_all('tr')[1:]:  # Omitir el encabezado
-        cells = row.find_all('td')
-        rows.append({headers[i+1]: cell.text for i, cell in enumerate(cells)})
-
+        cells = row.find_all('td')[:-1]  # Extraer todas las celdas de la fila
+        rows.append({headers[i]: cell.text.strip() for i, cell in enumerate(cells)})
+   
     # Guardar los datos en DynamoDB
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('TablaWebScrapping')
+    dynamodb_table = dynamodb.Table('TablaWebScrappingSismos')
 
     # Eliminar todos los elementos de la tabla antes de agregar los nuevos
-    scan = table.scan()
-    with table.batch_writer() as batch:
+    scan = dynamodb_table.scan()
+    with dynamodb_table.batch_writer() as batch:
         for each in scan['Items']:
             batch.delete_item(
                 Key={
@@ -54,8 +76,8 @@ def lambda_handler(event, context):
     for row in rows:
         row['#'] = i
         row['id'] = str(uuid.uuid4())  # Generar un ID único para cada entrada
-        table.put_item(Item=row)
-        i = i + 1
+        dynamodb_table.put_item(Item=row)
+        i += 1
 
     # Retornar el resultado como JSON
     return {
